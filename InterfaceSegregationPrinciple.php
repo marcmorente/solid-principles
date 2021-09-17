7fbc56210751c58a840556e0548794a99f9f092d
Marc Morente <mmorente@inthinking.co.uk>
2021-09-17 13:20:19 +0200

    first commit

---
+++ InterfaceSegregationPrinciple.php
@@ -0,0 +1,46 @@
+<?php
+
+interface Animals {
+    public function run();
+}
+
+interface Birds {
+	public function fly();
+}
+
+interface Cans {
+	public function bark();
+}
+
+
+class Dog implements Animals, Cans {
+
+
+    public function run() {
+        echo "Dog is running\n";
+    }
+
+    public function bark() {
+        echo "Dog is barking\n";
+    }
+}
+
+
+class Bird implements Animals, Birds {
+    
+    public function run() {
+        echo "Bird is running\n";
+    }
+    
+    public function fly() {
+        echo "Bird is flying\n";
+    }
+}
+
+$bird = new Bird();
+$bird->run();
+$bird->fly();
+
+$dog = new Dog();
+$dog->run();
+$dog->bark();
